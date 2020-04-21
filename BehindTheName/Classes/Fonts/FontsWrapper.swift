b9d91fa399e918556685f904dcf22333be141dd4
victorapp12 <victor.perdomo12@gmail.com>
2020-04-21 16:57:00 +0200

    Custom fonts wrapper

--- BehindTheName/Classes/Fonts/FontsWrapper.swift
+++ BehindTheName/Classes/Fonts/FontsWrapper.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Victor Perdomo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class SantanderFont {
+    
+    public class func bold(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .bold)
+    }
+    
+    public class func semiBold(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .semibold)
+    }
+    
+    public class func light(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .light)
+    }
+    
+    public class func regular(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .regular)
+    }
+    
+}
